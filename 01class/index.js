8c1d7ebb5c1c1003950da164f52123ccbb68a2b8
rajendra0968jangid <rajendra0968jangid@gmail.com>
2024-07-25 16:28:30 +0530

    changes

---
+++ 01class/index.js
@@ -0,0 +1,55 @@
+// var name = "hello"
+// console.log(name)
+
+// let name1 = "ham kamjor hai"
+// console.log(name1)
+
+// const name2 = "Now we are strong"
+// console.log(name2)
+
+// var num1 = 1000
+// console.log(num1)
+
+// let num2 = 2000
+// console.log(num2)
+
+// const num3 = 3000
+// console.log(num3)
+
+
+// var num4 = 1000.55
+// console.log(num4)
+
+// let num5 = 2000.88
+// console.log(num5)
+
+// const num6 = 3000.11
+// console.log(num6)
+
+// let name = "hello"
+// console.log(typeof name)
+// console.log(name)
+
+// let name1 = 'rajendra'
+// console.log(typeof name1)
+// console.log(name1)
+
+// let char1 = 'R'
+// console.log(typeof char1)
+// console.log(char1)
+
+// let num1 = 1000
+// console.log(typeof num1)
+// console.log(num1)
+
+// let num2 = 2000.33
+// console.log(typeof num2)
+// console.log(num2)
+
+// let bool = true
+// console.log(typeof bool)
+// console.log(bool)
+
+// let bool1 = false
+// console.log(typeof bool1)
+// console.log(bool1)
