bafab19184cdb6087fb76575684d6bfd81eb0540
rajendra0968jangid <rajendra0968jangid@gmail.com>
2024-07-25 16:27:31 +0530

    js second

---
+++ 02class/index.js
@@ -0,0 +1,56 @@
+// console.log("rajendra")
+// var name = "hello"
+// name = "world"
+// console.log(name)
+
+// let name1 = "hello1"
+// name1 = "world1"
+// console.log(name1)
+
+// const name2 = "hello2"
+// name2 = "world2"
+// console.log(name2)
+
+
+// var name1 = "hello1"
+// var name1 = "world1"
+// console.log(name1)
+
+// let name2 = "hello2"
+// let name2 = "world2"
+// console.log(name2)
+
+// const name3 = "hello3"
+// const name3 = "world3"
+// console.log(name3)
+
+// var name1 = "hello1"
+// let name1 = "hello2"
+// const name1 = "hello3"
+// console.log(name1)
+
+// const obj = {"name":"rajendra","age":20,"isLive":true}
+// console.log(obj)
+// console.log(obj["name"])
+// console.log(obj["age"])
+// console.log(obj["isLive"])
+
+
+// const obj1 = {"first name":"rajendra","last name":"jangid","address":"ajmer"}
+// obj1["address"] = "Jaipur"
+// obj1["village"] = "bandanwara"
+// console.log(obj1)
+
+// const arr = ["rajendra",24,"ajmer",true]
+// arr[0] = "hello"
+// console.log(arr)
+// console.log(arr[0])
+// console.log(arr[1])
+
+// let num1 = {"age":2,"age1":3}
+// let num2 = {"age":24,"age1":25}
+// console.log(num1["age"]+num2["age1"])
+let num1 = [20,60,80]
+let num2 = [2,4,8]
+console.log(num1[0]+num2[0])
+
